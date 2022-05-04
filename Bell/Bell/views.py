eccd6c846f599a5c873beec21e398650c1bcda0d
hamzaberradadev <5hamza.berr2012@gmail.com>
2022-05-03 21:00:08 -0400

    init_bell_work

---
+++ Bell/Bell/views.py
@@ -0,0 +1,5 @@
+import imp
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+def home(request):
+    return render(request, 'home.html')
---
+++ Bell/lib-dir/excel_interpret.py
@@ -0,0 +1,8 @@
+from openpyxl import load_workbook
+import pandas
+import os
+cwd = os.getcwd()  # Get the current working directory (cwd)
+#files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, ''))
+#df = load_workbook(filename=".test.xlsx")
+#print(df)
